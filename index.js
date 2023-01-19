de0c37eaba453ca0ab46328afac7eed5e1c48108
jamilahmed2 <ja093599@gmail.com>
2023-01-20 01:26:31 +0500

    Task No 2

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ let btn = document.querySelector('button');
 btn.addEventListener('click', chngWord);
 
 function chngWord() {
+   alert('Hello world')
+}
+
+// Task No 2 
+let btn = document.querySelector('button');
+btn.addEventListener('click', chngWord);
+
+function chngWord() {
    let word = prompt('Enter anything to replace');
    btn.textContent = " "+ word
 }
