d1765792ad3a6b514a1deb7afdfd6500cec0a049
jamilahmed2 <ja093599@gmail.com>
2023-01-20 01:34:47 +0500

    Task No 3

--- index.js
+++ index.js
@@ -14,3 +14,25 @@ function chngWord() {
    let word = prompt('Enter anything to replace');
    btn.textContent = " "+ word
 }
+
+
+// Task No 3
+
+// boolean
+var bool = true;
+console.log(typeof bool)
+// number
+var num = 1;
+console.log(typeof num)
+// string
+var num = "2";
+console.log(typeof num)
+// null
+var num = null;
+console.log(typeof num)
+// undefined
+var num;
+console.log(typeof num)
+// bigint
+var num = 12345n;
+console.log(typeof num)
